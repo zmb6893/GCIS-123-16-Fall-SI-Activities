c9a0630cf545aa1e5aea19c0e3364c31f5b52a8c
Zoe Bingham <zmb6893@g.rit.edu>
2021-09-02 10:39:56 -0400

    activity.py created

---
+++ week02/activity.py
@@ -0,0 +1,18 @@
+"""
+What goes here???
+"""
+
+# How do you define a function?
+
+# How do you receive input?
+
+# How do you print that output to the console?
+
+# What are the different data types and when do you use them?
+
+# How do you cast a data type to another data type?
+
+# How do you call a function?
+
+# How do you create a main function?
+
