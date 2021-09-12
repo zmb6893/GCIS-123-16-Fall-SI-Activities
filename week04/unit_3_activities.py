308f70d5d8ecc8fa8cbe9a1dae031f991332341d
Zoe Bingham <zmb6893@g.rit.edu>
2021-09-12 12:26:54 -0400

    created activity outline

---
+++ week04/unit_3_activities.py
@@ -0,0 +1,36 @@
+"""
+Michael's Session: Introducing unit testing, returns, conditionals, TDD, and pytest
+@author: Zoe Bingham
+"""
+
+# What should we name the test file? What do we import?
+
+def greeter(name):
+    '''
+    Says hello to a user given their name
+    '''
+
+def c2f(celsius):
+    '''
+    Takes a degree count in Celsius and converts it to Fahrenheit
+    '''
+
+def is_odd(n):
+    '''
+    Returns true if n is odd; otherwise, return false
+    '''
+
+def is_even(n):
+    '''
+    Returns true if n is even; otherwise, return false
+    '''
+
+def my_max(n1, n2):
+    '''
+    Takes two numbers and returns the greater one
+    '''
+
+def av_three(n1, n2, n3):
+    '''
+    Takes three numbers and returns the average
+    '''
