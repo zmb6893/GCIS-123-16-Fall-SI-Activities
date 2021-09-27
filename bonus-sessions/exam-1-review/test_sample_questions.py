f347a3bd32a654ed1b157a0e7eaab868f8daed35
Zoe Bingham <zmb6893@g.rit.edu>
2021-09-27 18:51:26 -0400

    failing

--- bonus-sessions/exam-1-review/test_sample_questions.py
+++ bonus-sessions/exam-1-review/test_sample_questions.py
@@ -13,3 +13,25 @@ def test_did_i_pass_70():
 
     # analyze
     assert expected == result
+
+def test_did_i_pass_69():
+    # setup
+    expected = False
+    score = 69
+
+    # invoke
+    result = sample_questions.did_i_pass(score)
+
+    # analyze
+    assert expected == result
+
+def test_did_i_pass_71():
+    # setup
+    expected = True
+    score = 71
+
+    # invoke
+    result = sample_questions.did_i_pass(score)
+
+    # analyze
+    assert expected == result
