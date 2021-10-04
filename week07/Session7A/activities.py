966a682808d83a735782a928443958df593cdbef
Zoe Bingham <zmb6893@g.rit.edu>
2021-10-04 19:22:40 -0400

    class solutions

--- week07/Session7A/activities.py
+++ week07/Session7A/activities.py
@@ -3,13 +3,25 @@ Session 7A: Making arrays,linear search, and timing functions
 """
 import starter_code.arrays as arrays
 import time
+import random
 
 def make_even_array(start, end):
     '''
     Make an array with even numbers from start to end
     '''
 
-    return
+    total_range = (end - start)
+
+    even_numbers = arrays.Array(int(total_range/2))
+
+    count = 0
+    for i in range(start,end):
+        if i % 2 == 0:
+            # add it to the array
+            even_numbers[count] = i
+            count += 1
+
+    return even_numbers
 
 def make_odd_array(start, end):
     '''
@@ -41,6 +53,13 @@ def linear_search(array, n):
     :param int array: array to be searched
     :param int n: the value to be found
     '''
+
+    # one by one search
+    for index in range(len(array)):
+        if array[index] == n:
+            # we found the number at index
+            return index
+
     return
 
 def random_array(size, seed):
@@ -51,9 +70,20 @@ def random_array(size, seed):
     :param int size: size of the array
     :param int seed: seed for random
     '''
-    return
+
+    random.seed(seed)
+
+    array = arrays.Array(size)
+
+    for i in range(size):
+        array[i] = random.randint(1, 1000)
+
+    return array
 
 def main():
-    print(time.perf_counter())
+    array = make_even_array(6, 42)
+    print(str(make_even_array(6, 42)))
+    print("Found at index: " + str(linear_search(array, 10)))
+    print(random_array(100, 4))
 
 main()
