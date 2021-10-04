7e19353566593f8c6f98bd726ce0cc0077bef945
Zoe Bingham <zmb6893@g.rit.edu>
2021-10-04 17:39:21 -0400

    activities for session 7B

---
+++ week07/Session7A/activities.py
@@ -0,0 +1,47 @@
+"""
+Session 7A: Making arrays,linear search, and timing functions
+"""
+import starter_code.arrays as arrays
+
+def make_even_array(start, end):
+    '''
+    Make an array with even numbers from start to end
+    '''
+
+    return
+
+def make_odd_array(start, end):
+    '''
+    Make an array with even numbers from start to end
+
+    Parameters:
+    :param int start: beginning of the array
+    :param int end: ending of the array
+    '''
+
+    return
+
+def print_every_nth_index(array, n):
+    '''
+    Print every nth index in an array. Example: array = [1,2,3,4,5,6,7] n = 2 output = [2,4,6]
+
+    Parameters:
+    :param int array: array to be printed
+    :param int n: the increment to be applied to the list
+    '''
+
+    return
+
+def linear_search(array, n):
+    '''
+    Search an array for the specified value using the linear search in an array
+
+    Parameters:
+    :param int array: array to be searched
+    :param int n: the value to be found
+    '''
+
+def main():
+    print()
+    
+main()
