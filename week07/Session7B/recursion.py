3f6b4c0b1cbed465e5a8e9c75683efc4a2e06726
Zoe Bingham <zmb6893@g.rit.edu>
2021-10-07 10:57:39 -0400

    Activities for Session7B created

---
+++ week07/Session7B/recursion.py
@@ -0,0 +1,49 @@
+"""
+Session 7B: Recursion activities
+"""
+
+import turtle as t
+
+def add_to_previous(start, depth):
+    '''
+    Implement a function that continues to add it's previous value until the specified depth is reached.
+    Return the value at depth
+
+    Parameters:
+    :param int start: starting number
+    :param int depth: when to end
+
+    :return int
+    '''
+
+def draw_tree_branch(size):
+    '''
+    Use turtle to draw a branch recursively.
+
+    Parameters:
+    :param int size: the scalar of the branch size
+    '''
+
+    return
+
+def draw_flower(size):
+    '''
+    Use turtle to draw a flower recursively
+
+    Parameters:
+    :param int size: the scalar of the flower size
+    '''
+    return
+
+def binary_search():
+    '''
+    Implement a binary search algorithm
+    '''
+    return
+
+
+def main():
+    draw_flower(1)
+
+if __name__ == "__main__":
+    main()
