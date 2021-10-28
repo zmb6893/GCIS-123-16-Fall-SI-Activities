e8ab3ae2ea624be7cdbbf5500a986eef50197749
zmb6893 <zmb6893@rit.edu>
2021-10-28 10:33:02 -0400

    activities for session 10B created

---
+++ week10/Session10B/activities.py
@@ -0,0 +1,65 @@
+"""
+Session 10B: List comprehension and sets
+"""
+
+CARDS = set()
+
+def create_card(suit, value):
+    """
+    Return a card of suit and value as a list of the name and value. Example: create_card("Hearts", 12) -> ["Queen of Hearts", 12]
+
+    parameters:
+    :param string suit: The suit
+    :param int value: The value of the card
+    """
+    return
+
+def fill_deck():
+    """
+    Create a full deck of cards using list comprehension. Return this list as a set.
+    """
+    return
+
+def sort_deck(deck):
+    """
+    Sort a deck of cards. Can you sort a set?
+
+    parameters:
+    :param set deck: a deck of cards
+    """
+    return
+
+def split_deck(n):
+    """
+    Split a deck into n sections.
+
+    parameters:
+    :param int n: The number of sections the deck will be split into
+    """
+    return
+
+def find_card_in_list(card, deck):
+    """
+    Find a card in the deck.
+
+    parameters:
+    :param list card: A card with name and value
+    :param list deck: A deck of cards
+    """
+    return
+
+def find_card_in_set(card, deck):
+    """
+    Find a card in the deck.
+
+    parameters:
+    :param list card: A card with name and value
+    :param set deck: A deck of cards
+    """
+    return
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
