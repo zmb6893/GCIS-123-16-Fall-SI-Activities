13122449bd7aeb06ba854805bc8c4b1d3b9aba67
zmb6893 <zmb6893@rit.edu>
2021-10-28 11:36:03 -0400

    updated activities

--- week10/Session10B/activities.py
+++ week10/Session10B/activities.py
@@ -2,23 +2,35 @@
 Session 10B: List comprehension and sets
 """
 
-CARDS = set()
+from functools import cmp_to_key
+
+FACE_CARDS = ["Jack", "Queen", "King", "Ace"]
+SUITS = ["Spades", "Hearts", "Clubs", "Diamonds"]
 
 def create_card(suit, value):
     """
-    Return a card of suit and value as a list of the name and value. Example: create_card("Hearts", 12) -> ["Queen of Hearts", 12]
+    Return a card of suit and value as a list of the name. Example: create_card("Hearts", 12) -> "Queen of Hearts"
 
     parameters:
     :param string suit: The suit
     :param int value: The value of the card
     """
-    return
 
-def fill_deck():
+    return 
+
+def fill_deck_set():
     """
     Create a full deck of cards using list comprehension. Return this list as a set.
     """
-    return
+
+    return 
+
+def fill_deck_list():
+    """
+    Create a full deck of cards using list comprehension. Return this list as a set.
+    """
+
+    return 
 
 def sort_deck(deck):
     """
@@ -27,16 +39,22 @@ def sort_deck(deck):
     parameters:
     :param set deck: a deck of cards
     """
-    return
+    return 
 
-def split_deck(n):
+def card_comparator(a, b):
+
+    return 
+
+def split_deck(deck, n):
     """
     Split a deck into n sections.
 
     parameters:
+    :param list deck: list of cards
     :param int n: The number of sections the deck will be split into
     """
-    return
+
+    return 
 
 def find_card_in_list(card, deck):
     """
@@ -46,7 +64,7 @@ def find_card_in_list(card, deck):
     :param list card: A card with name and value
     :param list deck: A deck of cards
     """
-    return
+    return 
 
 def find_card_in_set(card, deck):
     """
@@ -56,10 +74,22 @@ def find_card_in_set(card, deck):
     :param list card: A card with name and value
     :param set deck: A deck of cards
     """
-    return
+
+    return 
 
 def main():
-    pass
+    print(create_card("Hearts", 5))
+    print()
+
+    deck_set = fill_deck_set()
+    deck_list = fill_deck_list()
+
+    print(deck_set)
+    print(sort_deck(deck_set))
+    print(find_card_in_set("Queen of Hearts", deck_set))
+    print(find_card_in_list("Queen of Hearts", deck_list))
+    print(split_deck(deck_list, 3))
+
 
 if __name__ == "__main__":
     main()
