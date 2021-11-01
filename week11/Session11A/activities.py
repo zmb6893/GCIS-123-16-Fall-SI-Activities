ef748a5f45c950146209d20b33619e4aa76ee750
zmb6893 <zmb6893@rit.edu>
2021-11-01 17:13:54 -0400

    activities created

---
+++ week11/Session11A/activities.py
@@ -0,0 +1,38 @@
+"""
+Session 11A: Dictionaries and some hash stuff
+"""
+
+# What are 3 things to consider while making a good hash?
+
+# Why do we use hashes?
+
+def make_word_dictionary():
+    """
+    Make a dictionary using a word as a key and it's definition as the value.
+    """
+    return
+
+def make_chess_pieces(csv_file):
+    """
+    Return a dictionary for chess pieces. The key should be the name, the value should be the count from a given csv_file
+    """
+    return 
+
+def place_pieces_on_board(dictionary):
+    """
+    Challenge: Use list comprehension to make an 8X8 board and place the chess pieces in their respective locations.
+    Return the 2D list.
+    """
+    return
+
+def chess_hash(piece):
+    """
+    Make a hash for a chess piece with no conditional statements
+    """
+    return
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
