1030827bdd41802bad5c9af5dd3f647c813c9d0c
zmb6893 <zmb6893@rit.edu>
2021-11-04 13:32:00 -0400

    Zoe's person created

--- week11/Session11B/activities.py
+++ week11/Session11B/activities.py
@@ -14,7 +14,7 @@ class Person:
 # Once you have completed the above step, add a state for courses to the person class
 
 # Make a class for a favorite hobby that has fields for hobby_name and description
-# Once you've have completed this step, add a state for favorite hobby to the person class
+# Once you've have completed this step, add the state for favorite hobby to the person class
 class FavoriteHobby:
     hobby_name = "hobby"
     description = "description"
@@ -32,21 +32,41 @@ def main():
     John.mood = "Happy"
 
     # make a favorite hobby for John
-    favoriteHobby = FavoriteHobby()
-    favoriteHobby.hobby_name = "Soccer"
-    favoriteHobby.description = "A game where players kick a ball and score goals"
-    John.favoriteHobby = favoriteHobby
+    soccer = FavoriteHobby()
+    soccer.hobby_name = "Soccer"
+    soccer.description = "A game where players kick a ball and score goals"
+    John.favoriteHobby = soccer
     print("Person: %s\n"%John)
     print("Person: name: %s\tmood: %s\tage: %d"%(John.name, John.mood, John.age))
     print("\t%s's favorite hobby is %s: %s\n"%(John.name, John.favoriteHobby.hobby_name, John.favoriteHobby.description))
 
-    John.name = "AJ"
-    John.age = -4
-    John.mood = "DEPRESSED"
+    AJ = Person()
+    AJ.name = "AJ"
+    AJ.age = -4
+    AJ.mood = "DEPRESSED"
+
+
+    Zoe = Person()
+    Zoe.name = "Zoe"
+    Zoe.age = 19
+    Zoe.mood = "Tired"
+
+
+    # make AJ's favorite hobby
+    sleep = FavoriteHobby()
+    sleep.hobby_name = "Sleeping"
+    sleep.description = "Going to bed"
+
+    # change the state of AJ's hobby to his hobby
+    AJ.favoriteHobby = sleep
+    print("\t%s's favorite hobby is %s: %s\n"%(AJ.name, AJ.favoriteHobby.hobby_name, AJ.favoriteHobby.description))
 
     # print the instance of this class. what happens?
-    print("Person: %s\n"%John)
-    print("Person: name: %s\tmood: %s\tage: %d\n"%(John.name, John.mood, John.age))
+    Zoe.favoriteHobby = sleep
+    print("Person: %s\n"%Zoe)
+    print("Person: name: %s\tmood: %s\tage: %d\n"%(Zoe.name, Zoe.mood, Zoe.age))
+    print("\t%s's favorite hobby is %s: %s\n"%(Zoe.name, Zoe.favoriteHobby.hobby_name, Zoe.favoriteHobby.description))
+
 
     # print the individual fields of the class. what happens?
 
