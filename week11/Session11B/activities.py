e1891a3d2bba33e565f6395923bdb049a5d6aa8d
zmb6893 <zmb6893@rit.edu>
2021-11-04 11:17:04 -0400

    Session11B activities created

---
+++ week11/Session11B/activities.py
@@ -0,0 +1,24 @@
+"""
+Session 11B: Classes
+"""
+
+# Make a person class that holds the state for a name, age, and mood
+
+# Make a course class that holds the state for course_name, course_number, and number_of_students
+# Once you have completed the above step, add a state for courses to the person class
+
+def main():
+    # instantiate the person class with your name, age, and mood
+
+    # print the instance of this class. what happens?
+    # print the individual fields of the class. what happens?
+
+    # instantiate the course class with the GCIS-123 class information
+    # add this course to the person class
+
+    # are there any other courses you know the information for? instantiate it.
+
+    pass
+
+if __name__ == "__main__":
+    main()
