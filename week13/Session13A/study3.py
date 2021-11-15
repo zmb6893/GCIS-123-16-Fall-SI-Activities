7acf2659de8bb7282894c3268f4500cfdd8dcc88
zmb6893 <zmb6893@rit.edu>
2021-11-15 15:54:42 -0500

    working on solutions

--- week13/Session13A/study3.py
+++ week13/Session13A/study3.py
@@ -6,32 +6,32 @@ def make_tuple(a,b,c):
     """
     Returns a tuple containing all three parameters.
     """
-    pass
+    return a,b,c
 
 def reverse_tuple(sequence):
     """
     Returns a tuple containing the values in the sequence in reverse order.
     """
-    pass
+    return sequence[::-1]
 
 def make_list(a,b,c):
     """
     Returns a list containing all three parameters
     """
-    pass
+    return [a,b,c]
 
 def nth_list(sequence, n):
     """
     Return a list that has every nth value from the sequence
-    """
-    pass
+    """    
+    return sequence[::n]
 
 def splice(a,b):
     """
     The function should permanently add all of the elements from list b to list a. Do not return a value.
     """
-    pass
-
+    a += b
+    
 def odds_before_evens(a_list):
     """
     Rearrange the elements in the list so that all of the odd values come before the even values. Otherwise, the values should remain in the same relative order.Do not return a value - modify the list inplace
@@ -114,21 +114,88 @@ def frequencies(counts):
     """
     pass
 
+def format_string(string):
+    """
+    Formats the print of an activity
+    """
+    print("\t%s"%(str(string)))
+
 def main():
     # Call your make_tuple function at least twice with different arguments and print the tuples that are returned.
+    print("Activity 1:")
+    format_string(make_tuple(1,3,2))
+    format_string(make_tuple(4,3,5))
+    print()
+
     # Call reverse_tuple from main with at least two different kinds of sequences and print the tuple that is returned
+    print("Activity 2:")
+    format_string(reverse_tuple([1,2,3,4,5]))
+    format_string(reverse_tuple((1,2,4,5,8,2)))
+    print()
     # Call your make_list function from main at least twice with different arguments and print the lists that are returned
+    print("Activity 3:")
+    format_string(make_list(3,6,2))
+    format_string(make_list('3',6,2.0))
+    print()
     # Call your function from main with at least two different sequences and print the lists that are returned
+    print("Activity 4:")
+    format_string(nth_list([0,1,2,3,4,5,6,7,8,9],3))
+    format_string(nth_list([1,2,3,4,5,6,7,8,9,0],3))
+    print()
+    
     # Print list a before and after the splice function is called
+    print("Activity 5:")
+    a = [1,2,3]
+    b = ["a","b","c"]
+    format_string(a)
+    format_string(splice(a,b))
+    print()
+    
     # Call your odds_before_evens function and print before and after
+    print("Activity 6:")
+    print()
+
     # Call the bisect function and print the results
+    print("Activity 7:")
+    print()
+
     # Call equivalent and print the results
+    print("Activity 8:")
+    print()
+
     # Call domino_to_string on two different dominos
+    print("Activity 9:")
+    print()
+    
     # Call is match on a few dominos
+    print("Activity 10:")
+    print()
+
     # Call list_range and print
+    print("Activity 11:")
+    print()
+
     # Call multiples on a sequence
+    print("Activity 13:")
+    print()
+
     # Call only_vowels 
+    print("Activity 14:")
+    print()
+
     # Call random_count
+    print("Activity 15:")
+    print()
+
     # Call disjoint
+    print("Activity 16:")
+    print()
+
     # Use the dictionary in your main function to determine the total number of times random.randint was called, the maximum number of times that any value was generated, and the sorted list of values that were generated the maximum number of times. 
+    print("Activity 17:")
+    print()
+
     pass
+
+if __name__ == "__main__":
+    main()
