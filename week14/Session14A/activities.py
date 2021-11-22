258e9b7c6de7550a1a47f16e82a2452b0cb94ec4
zmb6893 <zmb6893@g.rit.edu>
2021-11-22 15:11:55 -0500

    Activity outline for 14A:

--- week14/Session14A/activities.py
+++ week14/Session14A/activities.py
@@ -1,8 +1,32 @@
 """
-Session 14A: Stacks and Queues
+Session 14A: Stacks and Queues with pancakes
 """
 
+#TODO: Make a plate class that has a color
+class Plate:
+    pass
+
+
+#TODO: Make a pancake class that has toppings and acts as a node
+class Pancake:
+    pass
+
+
+#TODO: Make a class for a stack for pancakes. The first value should always be a plate. You should be able to pop, push, and peek
+class Stack:
+    pass
+
+#TODO: Make a queue class for plates. When a pancake stack is create, pop a plate from the queue. When a pancake stack has no more pancakes, push the plate back onto the queue.
+class Queue:
+    pass
+
 def main():
+    #TODO: Create five pancakes
+    #TODO: Add the pancakes to a stack of pancakes
+    #TODO: Peek to see what the top pancake's toppings are
+    #TODO: Eat the top pancake by popping from the pancake stack
+    #TODO: Add two more pancakes to the stack.
+    #TODO: Pop all the pancakes until you only have a plate. Add the plate to the queue
     pass
 
 if __name__ == "__main__":
