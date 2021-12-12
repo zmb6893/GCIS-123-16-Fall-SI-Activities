e87524e53e1c493f1e5e1a88290e72ffde244986
zmb6893 <zmb6893@rit.edu>
2021-12-12 16:58:56 -0500

    Testing activity

--- final-review/Test-Example/flower.py
+++ final-review/Test-Example/flower.py
@@ -1,39 +1,80 @@
 import turtle as t
 
 def draw_petal(size, color="red"):
+    # Prepare turtle
     t.tracer(False)
-    t.color("red")
+    t.color(color)
     t.pendown()
     t.begin_fill()
 
+    # initialize a variable for the tip coordinates
     tip_coord = None
 
+    # draws the petal
     for i in range(4):
+        # records the tip
         if i == 1:
             tip_coord = int(t.xcor()), int(t.ycor())
         t.forward(size)
         t.left(90)
 
+    # resets turtle
     t.end_fill()
     t.penup()
+
+    # returns the coordinate of the tip
     return tip_coord
 
-def draw_flower(size, petals, color="red"):
+def draw_flower_layer(size, petals, color="red"):
+    # determines the angle for separation between petals
     angle = 360/petals
 
+    # list to keep track of the tips in a flower
     tips = []
 
+    # draws the flower and adds to the list of tips
     for i in range(petals):
         tips.append(draw_petal(size, color))
         t.left(angle)
         t.goto(0,0)
         
-
+    # return the out put as a tuple of tuples
     return tuple(tips)
 
+def draw_flower(size, layers, color="red", alternate="pink", coords=list()):
+    # base case
+    if layers <= 1:
+        draw_flower_layer(size, 3, color) 
+        return
+
+    # recursive call
+    if (layers % 2):
+        coords.append(draw_flower_layer(size, fibonacci(layers+3), color))
+    else:
+        coords.append(draw_flower_layer(size, fibonacci(layers+3), alternate))
+    draw_flower(size*.85, layers-1, color, alternate, coords)
+    return tuple(coords)
+
+def fibonacci(n):
+    # base case
+    if n <= 1:
+        return n
+    else:
+        # recursive call
+        return(fibonacci(n-1) + fibonacci(n-2))
+
+def draw_and_print_flower(size, layers, color="red", alternate="pink"):
+    # draw the flower and get the coords
+    coords = draw_flower(size, layers, color, alternate)[0]
+
+    # Make a string using list comprehension and string concatenation
+    string = "\n"
+    [string:=(string+"\t\t"+str(coord)+"\n") for coord in coords]
+    print("A red flower of size 30 is being drawn with 3 layers…\n\tLocation of each tip:"+string)
+
 def main():
-    #print(draw_petal(100, "red"))
-    print(draw_flower(100,5))
+    # Example:
+    print(draw_and_print_flower(100,3,"blue","violet"))
     input()
 
 if __name__ == "__main__":
--- final-review/Test-Example/flower_test.py
+++ final-review/Test-Example/flower_test.py
@@ -33,26 +33,50 @@ def test_petal_size_100():
     #Assert
     assert expected == actual
 
-def test_draw_flower_6_petals():
+def test_draw_flower_layer_6_petals():
     #Setup
     expected = ((100, 0), (50, 86), (-49, 86), (-100, 0), (-50, -86), (49, -86))
     size = 100
     petals = 6
 
     #Invoke
-    actual = draw_flower(size, petals)
+    actual = draw_flower_layer(size, petals)
 
     #Assert
     assert expected == actual
 
-def test_draw_flower_5_petals():
+def test_draw_flower_layer_5_petals():
     #Setup
     expected = ((100, 0), (30, 95), (-80, 58), (-80, -58), (30, -95))
     size = 100
     petals = 5
 
     #Invoke
-    actual = draw_flower(size, petals)
+    actual = draw_flower_layer(size, petals)
 
     #Assert
+    assert expected == actual
+
+def test_draw_flower_size_100_layers_5():
+    #Setup
+    expected = (((100, 0), (95, 29), (82, 56), (62, 78), (36, 93), (7, 99), (-22, 97), (-49, 86), (-73, 68), (-90, 43), (-98, 14), (-98, -14), (-90, -43), (-73, -68), (-50, -86), (-22, -97), (7, -99), (36, -93), (62, -78), (82, -56), (95, -29)), ((85, 0), (75, 39), (48, 69), (10, 84), (-30, 79), (-63, 56), (-82, 20), (-82, -20), (-63, -56), (-30, -79), (10, -84), (48, -69), (75, -39)), ((72, 0), (51, 51), (0, 72), (-51, 51), (-72, 0), (-51, -51), (0, -72), (51, -51)), ((61, 0), (18, 58), (-49, 36), (-49, -36), (18, -58)))
+    size = 100
+    layers = 5
+
+    #invoke
+    actual = draw_flower(size, layers)
+
+    #analyze
+    assert expected == actual
+
+def test_draw_flower_size_10_layers_3():
+    #Setup
+    expected = (((10, 0), (7, 7), (0, 10), (-7, 7), (-10, 0), (-7, -7), (0, -10), (7, -7)), ((8, 0), (2, 8), (-6, 4), (-6, -4), (2, -8)))
+    size = 10
+    layers = 3
+
+    #invoke
+    actual = draw_flower(size, layers)
+
+    #analyze
     assert expected == actual
